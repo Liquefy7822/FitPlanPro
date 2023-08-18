716a99b53eb6db162d93fdf17f7aa620ccfd9284
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-18 09:17:09 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,10 @@ import Image from 'next/image';
 
 export default function Header({ title }) {
   return (
+    <header className="header">
       <div className="header-content">
         <Image src="https://i.imgur.com/SqwGFl8.jpg" alt="App Logo" width={80} height={80} />
-        <h1 className="FitPlanPro">{title}</h1>
+        <h1 className="app-title">{title}</h1>
         <a href="https://github.com/Liquefy7822/FitPlanPro">
           <Image src="/github-logo.svg" alt="GitHub" width={40} height={40} />
         </a>
