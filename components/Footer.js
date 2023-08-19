e4fdf2be99769d99294b4accf85f2fe5cb805f0e
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-19 19:37:57 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,22 @@
-import styles from './Footer.module.css'
+import { useRouter } from 'next/router';
+import styles from './Footer.module.css';
 
 export default function Footer() {
+  const router = useRouter();
+
+  const goBack = () => {
+    router.back();
+  };
+
   return (
     <>
       <footer className={styles.footer}>
         <img src="/HowTechLogo.jpg" alt="HowTech Logo" className={styles.logo} />
+        <button onClick={goBack} className={styles.goBackButton}>
+          Go Back
+        </button>
       </footer>
     </>
-  )
+  );
 }
+
