b018acd626c410d29a356b7f0d0d2f5b722fe515
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-19 12:08:05 +0800

    Update goal-setting.js

--- pages/goal-setting.js
+++ pages/goal-setting.js
@@ -1,1 +1,29 @@
 
+// pages/goal-setting.js
+import React from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import GoalSettingForm from '@components/GoalSettingForm';
+
+export default function GoalSetting() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Goal Setting - Your Fitness App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Goal Setting" />
+        <p className="description">
+          Upon sign-up, you can input your fitness goals and other information.
+        </p>
+
+        <GoalSettingForm />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
