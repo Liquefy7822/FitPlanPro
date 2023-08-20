01ce20c3d4fa8ed3504b47a61cb6c8845e7f48a4
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-20 15:16:43 +0800

    Update ExerciseLibrary.js

--- pages/ExerciseLibrary.js
+++ pages/ExerciseLibrary.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 function ExerciseLibrary() {
-  const [exerciseList, setExerciseList] = useState([]);
+  const [exerciseList, setExerciseList] = useState({});
 
   useEffect(() => {
     // Load the exercise list when the component mounts
@@ -19,26 +19,24 @@ function ExerciseLibrary() {
   return (
     <div>
       <h1>Exercise Library</h1>
-      <ul>
-        {Object.keys(exerciseList).map((category) => (
-          <li key={category}>
-            <h2>{category}</h2>
-            <ul>
-              {exerciseList[category].map((exercise) => (
-                <li key={exercise.name}>
-                  <h3>{exercise.name}</h3>
-                  <p>Description: {exercise.description}</p>
-                  <img src={exercise.image} alt={exercise.name} />
-                  <video controls>
-                    <source src={exercise.video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      {Object.keys(exerciseList).map((category) => (
+        <div key={category}>
+          <h2>{category}</h2>
+          <ul>
+            {exerciseList[category].map((exercise, index) => (
+              <li key={index}>
+                <h3>{exercise.name}</h3>
+                <p>Description: {exercise.description}</p>
+                <img src={exercise.image} alt={exercise.name} />
+                <video controls>
+                  <source src={exercise.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
