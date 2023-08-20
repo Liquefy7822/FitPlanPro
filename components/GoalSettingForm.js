0cd695845a2ca88ba0f03d1394a77d11d741df12
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-20 16:15:03 +0800

    Update GoalSettingForm.js

--- components/GoalSettingForm.js
+++ components/GoalSettingForm.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router'; // Import useRouter from Next.js
+import React, { useState } from 'react';
+import Link from 'next/link'; // Import Link from Next.js
 
 function GoalSettingForm() {
   const [formData, setFormData] = useState({
@@ -8,8 +8,7 @@ function GoalSettingForm() {
     fitness_level: '',
     time_available: 0,
   });
-
-  const router = useRouter(); // Initialize the router
+  const [workoutPlan, setWorkoutPlan] = useState([]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -23,10 +22,20 @@ function GoalSettingForm() {
     e.preventDefault();
 
     try {
-      // Collect and process form data here as before
+      const response = await fetch('/generate-workout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
 
-      // Redirect to the personalized workout page
-      router.push('/personalized-workout');
+      if (response.ok) {
+        const data = await response.json();
+        setWorkoutPlan(data.workout_plan);
+      } else {
+        console.error('Error generating workout plan');
+      }
     } catch (error) {
       console.error('Error:', error);
     }
@@ -35,9 +44,58 @@ function GoalSettingForm() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        {/* Your form input fields */}
+        <div>
+          <label htmlFor="goals">Fitness Goals:</label>
+          <select
+            name="goals"
+            id="goals"
+            multiple
+            onChange={handleInputChange}
+          >
+            <option value="strength">Strength</option>
+            <option value="flexibility">Flexibility</option>
+            <option value="endurance">Endurance</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            name="age"
+            id="age"
+            value={formData.age}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="fitness_level">Fitness Level:</label>
+          <select
+            name="fitness_level"
+            id="fitness_level"
+            value={formData.fitness_level}
+            onChange={handleInputChange}
+          >
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="time_available">Time Available (minutes):</label>
+          <input
+            type="number"
+            name="time_available"
+            id="time_available"
+            value={formData.time_available}
+            onChange={handleInputChange}
+          />
+        </div>
         <button type="submit">Generate Workout Plan</button>
       </form>
+      {/* Link to the personalized workout page */}
+      <Link href="/personalized-workout">
+        <button>Go to Personalized Workout Page</button>
+      </Link>
     </div>
   );
 }
