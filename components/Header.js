256ad4e3dbdbc954a22e1fa1c3536978794cb3f6
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-22 13:35:03 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,38 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="header">
-      {/* No content inside the header */}
+      // Function to save user data to local storage
+function saveUserData(data) {
+  localStorage.setItem('userData', JSON.stringify(data));
+}
+
+// Function to retrieve user data from local storage
+function getUserData() {
+  const storedData = localStorage.getItem('userData');
+  return storedData ? JSON.parse(storedData) : null;
+}
+
+// Function to remove user data from local storage
+function clearUserData() {
+  localStorage.removeItem('userData');
+}
+
+// Example usage:
+// Save data
+const userData = { username: 'john_doe', email: 'john@example.com' };
+saveUserData(userData);
+
+// Retrieve data
+const savedUserData = getUserData();
+if (savedUserData) {
+  console.log('User data:', savedUserData);
+} else {
+  console.log('User data not found.');
+}
+
+// Remove data
+// clearUserData();
+
     </header>
   );
 }
