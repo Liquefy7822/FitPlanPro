4485937d4a24e25d2b5328af7c4054df8b9daaac
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-22 18:54:52 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '@styles/globals.css'
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkLoaded } from "@clerk/clerk-react";
+import { useRouter } from "next/router";
+
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
