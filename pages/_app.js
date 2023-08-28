2d51585e75ef951372352cd043908f4395c40cc3
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-08-28 16:35:45 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import '@styles/globals.css'
 import { useRouter } from "next/router";
+import { SessionProvider } from "next-auth/react"
 
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
